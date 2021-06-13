2874d33644a833bca00b04145b7dd3452f709a24
promehedi <mmhasan563@gmail.com>
2021-06-13 19:13:44 +0600

    Create Filterable API URL

--- src/App.js
+++ src/App.js
@@ -7,8 +7,20 @@ import Search from './components/Search'
 const App = () => {
   const [recipes, setRecipes] = useState({})
   const [query, setQuery] = useState('chicken')
+  const [filter, setFilter] = useState({
+    perPage: 6,
+    diet: 'balanced',
+    health: 'alcohol-free',
+    cousineType: 'indian',
+    mealType: 'breakfast',
+    dishType: 'dessert',
+    ingredients: 5,
+    caloriesFrom: 0,
+    caloriesTo: 500,
+    time: 0,
+  })
 
-  const API_URL = `https://api.edamam.com/search?q=${query}&app_id=${process.env.REACT_APP_EDAMAM_APP_ID}&app_key=${process.env.REACT_APP_EDAMAM_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+  const API_URL = `https://api.edamam.com/search?q=${query}&app_id=${process.env.REACT_APP_EDAMAM_APP_ID}&app_key=${process.env.REACT_APP_EDAMAM_APP_KEY}&from=0&to=${filter.perPage}&calories=${filter.caloriesFrom}-${filter.caloriesTo}&health=${filter.health}&dishType=${filter.dishType}&mealType=${filter.mealType}&cousineType=${filter.cousineType}&diet=${filter.diet}&ingr=${filter.ingredients}&time=${filter.time}`
 
   useEffect(() => {
     const getRecipes = async () => {
@@ -19,11 +31,12 @@ const App = () => {
   }, [API_URL])
 
   console.log(recipes)
+  console.log(API_URL)
 
   return (
     <Layout>
       <Search query={query} setQuery={setQuery} />
-      <Filter />
+      <Filter filter={filter} setFilter={setFilter} />
       <div></div>
     </Layout>
   )
