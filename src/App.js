4d84ef245ac20bdfd8f8521c52f8165fd3f33060
promehedi <mmhasan563@gmail.com>
2021-06-13 14:52:37 +0600

    Setup Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h1>Recipe Search</h1>
+    <div className='text-center'>
+      <h1 className='font-bold text-5xl text-red-700'>Recipe Search</h1>
     </div>
   )
 }
