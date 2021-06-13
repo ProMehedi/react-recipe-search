f7c8ba9c27c01650111017bee611d1ac2b897cce
promehedi <mmhasan563@gmail.com>
2021-06-13 21:19:54 +0600

    Minimal Search Form

--- src/App.js
+++ src/App.js
@@ -1,43 +1,33 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import Filter from './components/Filter'
 import Layout from './components/Layout'
 import Search from './components/Search'
 
 const App = () => {
   const [recipes, setRecipes] = useState({})
-  const [query, setQuery] = useState('chicken')
-  const [filter, setFilter] = useState({
-    perPage: 6,
+  const [search, setSearch] = useState({
+    query: 'chicken',
     diet: 'balanced',
-    health: 'alcohol-free',
-    cousineType: 'indian',
     mealType: 'breakfast',
-    dishType: 'dessert',
-    ingredients: 5,
-    caloriesFrom: 0,
-    caloriesTo: 500,
-    time: 0,
   })
 
-  const API_URL = `https://api.edamam.com/search?q=${query}&app_id=${process.env.REACT_APP_EDAMAM_APP_ID}&app_key=${process.env.REACT_APP_EDAMAM_APP_KEY}&from=0&to=${filter.perPage}&calories=${filter.caloriesFrom}-${filter.caloriesTo}&health=${filter.health}&dishType=${filter.dishType}&mealType=${filter.mealType}&cousineType=${filter.cousineType}&diet=${filter.diet}&ingr=${filter.ingredients}&time=${filter.time}`
+  const API_URL = `https://api.edamam.com/search?q=${search.query}&app_id=${process.env.REACT_APP_EDAMAM_APP_ID}&app_key=${process.env.REACT_APP_EDAMAM_APP_KEY}&from=0&to=9&mealType=${search.mealType}&diet=${search.diet}`
 
-  useEffect(() => {
-    const getRecipes = async () => {
-      const { data } = await axios.get(API_URL)
-      setRecipes(data)
-    }
-    getRecipes()
-  }, [API_URL])
+  const submitHandler = async (e) => {
+    e.preventDefault()
 
-  console.log(recipes)
-  console.log(API_URL)
+    const { data } = await axios.get(API_URL)
+    setRecipes(data)
+    console.log(API_URL)
+  }
 
   return (
     <Layout>
-      <Search query={query} setQuery={setQuery} />
-      <Filter filter={filter} setFilter={setFilter} />
-      <div></div>
+      <Search
+        submitHandler={submitHandler}
+        search={search}
+        setSearch={setSearch}
+      />
     </Layout>
   )
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,23 +1,74 @@
 import React from 'react'
 
-const Search = ({ query, setQuery }) => {
+const Search = ({ search, setSearch, submitHandler }) => {
   return (
-    <>
-      <div className='flex items-center '>
+    <form onSubmit={submitHandler} className='flex flex-wrap items-end'>
+      <div className='flex w-full mb-3 md:mb-0 md:border-red-100 md:w-5/12 items-center'>
         <label htmlFor='search' className='absolute p-3 text-gray-500'>
           <i className='fa fa-search'></i>
         </label>
         <input
-          className='bg-transparent pl-9 pr-4 py-2 w-full bg-gray-200 rounded border border-gray-300 duration-500 focus:border-blue-400'
+          className='bg-transparent pl-9 pr-4 py-1 w-full rounded border border-gray-300 duration-500 focus:border-blue-400'
           type='search'
           name='search'
           id='search'
-          placeholder='Search recipies by name or keyworks... (banana)'
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
+          placeholder='Search by name or keywords... (banana)'
+          value={search.query}
+          onChange={(e) => setSearch({ ...search, query: e.target.value })}
         />
       </div>
-    </>
+      <div className='flex flex-wrap items-end w-full md:w-5/12 pl-0 md:pl-6'>
+        <div className='pr-5 mb-3 md:mb-0 w-1/2'>
+          <label htmlFor='diet' className='block mb-2 font-bold text-gray-600'>
+            Diet:
+          </label>
+          <select
+            name='diet'
+            id='diet'
+            className='border w-full px-3 py-1 rounded'
+            value={search.diet}
+            onChange={(e) => setSearch({ ...search, diet: e.target.value })}
+          >
+            <option value='balanced'>Balanced</option>
+            <option value='high-protein'>High Protein</option>
+            <option value='high-fiber'>High Fiber</option>
+            <option value='low-fat'>Low Fat</option>
+            <option value='low-carb'>Low Carb</option>
+            <option value='low-sodium'>Low Sodium</option>
+          </select>
+        </div>
+        <div className='pr-5 mb-3 md:mb-0 w-1/2'>
+          <label
+            htmlFor='mealType'
+            className='block mb-2 font-bold text-gray-600'
+          >
+            Meal Type:
+          </label>
+          <select
+            name='mealType'
+            id='mealType'
+            className='border w-full px-3 py-1 rounded'
+            value={search.mealType}
+            onChange={(e) => setSearch({ ...search, mealType: e.target.value })}
+          >
+            <option value='breakfast'>Breakfast</option>
+            <option value='lunch'>Lunch</option>
+            <option value='dinner'>Dinner</option>
+            <option value='snack'>Snack</option>
+            <option value='teatime'>Teatime</option>
+          </select>
+        </div>
+      </div>
+
+      <div className='w-full md:w-2/12 pl-0 md:pl-6'>
+        <button
+          typ='submit'
+          className='w-full py-2 px-3 bg-blue-600 text-white rounded'
+        >
+          Search
+        </button>
+      </div>
+    </form>
   )
 }
 
